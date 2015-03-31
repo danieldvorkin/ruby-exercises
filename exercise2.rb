052085c4723d855152f8d56a65e1ffc0640f9a54
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-03-31 10:11:28 -0400

    Added first line of code

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#Question 1
+#tip = 55 * 0.15
+#puts "Bill: $55"
+#puts "Tips: $#{tip}"
+#puts "Total: $#{tip + 55}"
+
+#Question 2
+#puts "daniel" + 2.to_s
+
+#Question 3
+a = 45628
+b = 7839
+
+puts "The value of a plus b is #{a * b}." 
