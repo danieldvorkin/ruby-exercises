18c6eb2bace3b1d83698fb7db7e4ceae9903c61c
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-01 10:46:24 -0400

    Finished exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,10 @@
 students = {
 	:cohort1 => 34,
-	:cohort2 => 42,
-	:cohort3 => 22
+	:cohort3 => 22,
+	:cohort4 => 43
 }
 
 students.each do |cohort, value|
-  puts "#{cohort}: #{value}"
+	value = value * 1.05
+  	puts "#{cohort}: #{value}"
 end
